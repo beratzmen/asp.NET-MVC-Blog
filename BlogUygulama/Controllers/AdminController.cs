902b917346442afe3e2c71de41ee2586c70b6387
Berat Ozmen <beratzmen@gmail.com>
2018-04-03 13:46:05 +0300

    Blog Uygulama 2

---
+++ BlogUygulama/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogUygulama.Models;
+
+
+namespace BlogUygulama.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        mvcblogDB db = new mvcblogDB();
+        public ActionResult Index()
+        {
+            ViewBag.makaleSayisi = db.Makales.Count();
+            ViewBag.kategoriSayisi = db.Kategoris.Count();
+            return View();
+        }
+    }
+}
---
+++ BlogUygulama/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogUygulama.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace BlogUygulama.Controllers
+{
+    public class HomeController : Controller
+    {
+        mvcblogDB db = new mvcblogDB();
+
+        // GET: Home
+        
+        public ActionResult Index(int Page=1)
+        {
+            var makale = db.Makales.OrderByDescending(m => m.MakaleId).ToPagedList(Page,5);
+            return View(makale);
+        }
+
+
+        public ActionResult BlogAra(string Ara=null)
+        {
+            var aranan = db.Makales.Where(m => m.Baslik.Contains(Ara)).ToList();
+            return View(aranan.OrderByDescending(m=>m.Tarih));
+        }
+
+
+
+        public ActionResult KategoriMakale(int id)
+        {
+            var makaleler = db.Makales.Where(m => m.Kategori.KategoriId == id).ToList();
+            return View(makaleler);
+        }
+
+
+
+        public ActionResult OkunmaArttir(int Makaleid)
+        {
+            var makale = db.Makales.Where(m => m.MakaleId == Makaleid).SingleOrDefault();
+            makale.Okunma += 1;
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult MakaleDetay(int id)
+        {
+            var makale = db.Makales.Where(m => m.MakaleId ==id ).SingleOrDefault();
+            if (makale==null)
+            {
+                return HttpNotFound();
+            }
+            return View(makale);
+        }
+        
+        public ActionResult Hakkımızda()
+        {
+            return View();
+        }
+        
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+        
+        public ActionResult Kategori()
+        {
+           
+            
+            return View( db.Kategoris.ToList());
+          
+        }
+      
+
+      
+    }
+}
